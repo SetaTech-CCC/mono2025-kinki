449e921744d9bad52e36cac78c26efa868654f07
Syuugo <pub@s1204.me>
2025-08-03 01:04:36 +0900

    全行にコメントを追加

--- Inspector/Inspector.ino
+++ Inspector/Inspector.ino
@@ -20,28 +20,33 @@ void loop() {
     dc(S);
     // 7セグと半固定抵抗を同期
     syncPot();
+  // トグルが上向きの時
   } else {
     // DCモーターを右回転
     dc(RT);
     // ステッピングモーターを右回転
     stepper();
   }
+  // 自作基盤上の右から２番目のタクトが押された時
   if (isTactPressed(RL) && bar_gage > 1) {
     // LEDバーの位置を下げる
     bar_gage--;
     // ブザー中音鳴動
     buzz(MI, 0.2);
+  // 自作基盤上の右端のタクトが押された時
   } else if (isTactPressed(RR) && bar_gage < 10) {
     // LEDバーの位置を上げる
     bar_gage++;
     // ブザー高音鳴動
     buzz(HI, 0.2);
   }
+  // 自作基盤上の左端のタクトが押された時
   if (isTactPressed(LL) && bar_color > 1) {
     // LEDバーの色を一つ前に戻す
     bar_color--;
     // ブザー中音鳴動
     buzz(MI, 0.2);
+  // 自作基盤上の左から２番目のタクトが押された時
   } else if (isTactPressed(LR) && bar_color < 8) {
     // LEDバーの色を一つ進める
     bar_color++;
@@ -52,10 +57,10 @@ void loop() {
   bar(lineIndex[bar_gage - 1], ledIndex[bar_color - 1]);
   // LEDマトリックスとジョイスティックを同期
   syncArrow();
-  // サーボ角度増加
-  if (isTactPressed(TL) && pos < SERVO_MAX) pos+=8;
-  // サーボ角度減少
-  if (isTactPressed(TR) && pos > SERVO_MIN) pos-=8;
+  // サーボ角度増加（大会基盤上の左側のタクトが押された時）
+  if (isTactPressed(TL) && pos < SERVO_MAX) pos += 8;
+  // サーボ角度減少（大会基盤上の右側のタクトが押された時）
+  if (isTactPressed(TR) && pos > SERVO_MIN) pos -= 8;
   // サーボ適用
   servo(pos);
 }
